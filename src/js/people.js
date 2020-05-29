27e762e2ad0aa9a9a3422905903604b2f917f5ff
brad Traversy <bra@tranversary.com>
2020-05-29 13:19:54 +0530

    first commit

---
+++ src/js/people.js
@@ -0,0 +1,12 @@
+/*function gethello(){
+    return "hello there";
+}
+*/
+
+let people =[
+    {name:"john doe"},
+    {name:"steve smith"},
+    {name: "carol williams"}
+];
+
+module.exports = people;
